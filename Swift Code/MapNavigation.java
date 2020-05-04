953b224a04ba73ccaf4dcc22dcd19f254b4b3c38
Kevin Choe <kevinkw.choe@gmail.com>
2020-05-03 23:17:05 -0700

    Create MapNavigation.java

---
+++ Swift Code/MapNavigation.java
@@ -0,0 +1,11 @@
+import UIKit
+
+class MapNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
